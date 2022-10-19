b73bc60ef9f1fcf1404e31dd8ec1ef5212c735e3
Cupid Valentine <westerngod@126.com>
2022-10-19 22:45:27 +0800

    feat: add four routes

---
+++ src/components/RedirectToWelcome.tsx
@@ -0,0 +1,21 @@
+import type { FC } from 'react'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+interface ErrorPageProps {}
+
+const ErrorPage: FC<ErrorPageProps> = () => {
+  // const error: any = useRouteError()
+
+  const nav = useNavigate()
+  useEffect(() => {
+    nav('/welcome/1')
+  }, [])
+
+  return null
+  // return (
+  //   <p>{`status text${error.statusText}` || `message==>${error.message}`}</p>
+  // )
+}
+
+export default ErrorPage
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import ErrorPage from './components/ErrorPage'
+import { NavLink, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import RedirectToWelcome from './components/RedirectToWelcome'
 // import App from './App'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>root</div>,
-    errorElement: <ErrorPage />,
+    element: (
+      <div>
+        root <Outlet />
+      </div>
+    ),
+    errorElement: <RedirectToWelcome />,
+    children: [
+      { index: true, element: <div>null</div> },
+      {
+        path: 'welcome',
+        element: <Outlet />,
+        children: [
+          { index: true, element: <div>null</div> },
+          { path: '1', element: <div>1  <NavLink to={'/welcome/2'}>下一页</NavLink> </div> },
+          { path: '2', element: <div>2  <NavLink to={'/welcome/3'}>下一页</NavLink> </div> },
+          { path: '3', element: <div>3  <NavLink to={'/welcome/4'}>下一页</NavLink> </div> },
+          { path: '4', element: <div>4  <NavLink to={'/welcome/5'}>下一页</NavLink> </div> },
+        ],
+      },
+    ],
+    // children: [
+    //   { index: true, element: <div>请选择1 2 3</div> }, // 路径为 /
+    //   { path: '1', element: <div>1</div> }, // 路径为 /1
+    //   { path: '2', element: <div>2</div> },
+    //   { path: '3', element: <div>3</div> },
+    // ],
   },
-  {
-    path: '/1',
-    element: <div>123</div>,
-  },
+  // {
+  //   path: '/4',
+  //   element: <div>root/4</div>,
+  // },
 ])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
