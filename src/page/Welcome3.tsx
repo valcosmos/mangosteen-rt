9cb1fd7aa6cd58f6257ad7626d029b488dc16e0a
Cupid Valentine <westerngod@126.com>
2022-10-22 00:13:25 +0800

    feat: try unocss

--- src/page/Welcome3.tsx
+++ src/page/Welcome3.tsx
@@ -5,7 +5,7 @@ interface Welcome1Props {}
 
 const Welcome3: FC<Welcome1Props> = () => {
   return (
-    <div flex justify-center items-center after="content-[123] b-1 b-red" before="content-[before] b-1 b-blue">
+    <div flex justify-center items-center shadow after="content-[123] b-1 b-red" before="content-[before] b-1 b-blue">
       <header hover:bg-red w-100px b-1 b-red h-40px></header>
 
       <main grow-1 b-1 b-blue h-100px></main>
