a5e60f9d37ed1f73688dc444157978345a10d2e1
Cupid Valentine <westerngod@126.com>
2022-10-21 22:13:54 +0800

    feat: optimized router animation

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,28 +1,30 @@
 import { animated, useTransition } from '@react-spring/web'
 import type { FC, ReactNode } from 'react'
+import { useRef } from 'react'
 import { useLocation, useOutlet } from 'react-router-dom'
 
 interface WelcomeLayoutProps {}
 
-const map: Record<string, ReactNode> = {}
-
 const WelcomeLayout: FC<WelcomeLayoutProps> = () => {
+  const map = useRef<Record<string, ReactNode>>({})
+
   const location = useLocation()
+
   const outlet = useOutlet()
   // map作为outlet缓存
-  map[location.pathname] = outlet
+  map.current[location.pathname] = outlet
   const transition = useTransition(location.pathname, {
-    from: { transform: 'translateX(100%)' },
+    from: { transform: location.pathname === '/welcome/1' ? 'translateX(0%)' : 'translateX(100%)' },
     enter: { transform: 'translateX(0%)' },
     leave: { transform: 'translateX(-100%)' },
     config: {
-      duration: 1000,
+      duration: 300,
     },
   })
   return transition((style, pathname) => (
     <animated.div style={style} key={pathname}>
       {/* <Outlet /> */}
-      <div style={{ textAlign: 'center' }}>{map[pathname]}</div>
+      <div style={{ textAlign: 'center' }}>{map.current[pathname]}</div>
     </animated.div>
   ))
 }
